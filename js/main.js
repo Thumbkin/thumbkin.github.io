8e821a58bbdb275e6b9389bdc325e02696f3d352
Thumbkin <thumbkin83@gmail.com>
2025-05-01 18:45:17 +0200

    Added premade test cases

--- js/main.js
+++ js/main.js
@@ -1,69 +1,117 @@
 let startingSituation = new StartingSituation();
 
 function resetHuidigeSituatie() {
-    // verwijder alle huidige processen
-    let usedProcessIds = startingSituation.getUsedProcessIds();
-	
-    for(let i = 0; i < usedProcessIds.length; i++) {
-        removeProcess(usedProcessIds[i]);
-    }
+    startingSituation = new StartingSituation();
+
+    document.getElementById("lijst_processen").innerHTML =
+        "<tr>" +
+            "<th>Proces</th>" +
+            "<th>Start op</th>" +
+            "<th>Lengte</th>" +
+            "<th></th>" +
+        "</tr>";
+
+    addNewProcessRow();
 }
 
-function laadTestStartSituatieKlas() {
+// laad een vooraf genmaakte situatie zoals in de slides
+function laadStartSituatie(type) {
     resetHuidigeSituatie();
-    
-    const test_situatie = new Map();
-    test_situatie.set("A", [0, 7]);
-    test_situatie.set("B", [1, 4]);
-    test_situatie.set("C", [2, 2]);
-    test_situatie.set("D", [3, 10]);
-    test_situatie.set("E", [4, 1]);
-    test_situatie.set("F", [5, 6]);
-    test_situatie.set("G", [7, 3]);
-    test_situatie.set("H", [20, 2]);
 
-    test_situatie.forEach(voegProcesToeUitTestStart)
+    const test_situatie = new Map();
+    if(type === "Klas") { laadStartSituatieKlas() };
+    if(type === "GrootNaarKlein") { laadStartSituatieGrootNaarKlein() };
+    if(type === "KleinNaarGroot") { laadStartSituatieKleinNaarGroot() };
+    if(type === "LangeProcessen") { laadStartSituatieLangeProcessen() };
+    if(type === "KorteProcessen") { laadStartSituatieKorteProcessen() };
+    if(type === "AlleKleuren") { laadStartSituatieAlleKleuren() };
 }
 
-function laadTestAlleKleuren() {    
-    resetHuidigeSituatie();
-    
-    const test_situatie = new Map();
-    test_situatie.set("A", [0, 7]);
-    test_situatie.set("B", [1, 4]);
-    test_situatie.set("C", [2, 2]);
-    test_situatie.set("D", [3, 10]);
-    test_situatie.set("E", [4, 1]);
-    test_situatie.set("F", [5, 6]);
-    test_situatie.set("G", [7, 3]);
-    test_situatie.set("H", [20, 2]);
-    test_situatie.set("I", [1, 4]);
-    test_situatie.set("J", [2, 2]);
-    test_situatie.set("K", [3, 10]);
-    test_situatie.set("L", [4, 1]);
-    test_situatie.set("M", [5, 6]);
-    test_situatie.set("N", [7, 3]);
-    test_situatie.set("O", [20, 2]);
-    test_situatie.set("P", [1, 4]);
-    test_situatie.set("Q", [2, 2]);
-    test_situatie.set("R", [3, 10]);
-    test_situatie.set("S", [4, 1]);
-    test_situatie.set("T", [5, 6]);
-    test_situatie.set("U", [7, 3]);
-    test_situatie.set("V", [20, 2]);
-    test_situatie.set("W", [3, 10]);
-    test_situatie.set("X", [4, 1]);
-    test_situatie.set("Y", [5, 6]);
-    test_situatie.set("Z", [7, 3]);
-
-    test_situatie.forEach(voegProcesToeUitTestStart)
+function laadStartSituatieKlas() {
+    voegStartProcesToe("A", 0, 7);
+    voegStartProcesToe("B", 1, 4);
+    voegStartProcesToe("C", 2, 2);
+    voegStartProcesToe("D", 3, 10);
+    voegStartProcesToe("E", 4, 1);
+    voegStartProcesToe("F", 5, 6);
+    voegStartProcesToe("G", 7, 3);
+    voegStartProcesToe("H", 20, 2);
 }
 
-function voegProcesToeUitTestStart(value, key, map){
-    document.getElementById("dd_start_" + key).value = value[0];
-    document.getElementById("dd_length_" + key).value = value[1];
+function laadStartSituatieGrootNaarKlein() {
+    voegStartProcesToe("A", 0, 12);
+    voegStartProcesToe("B", 1, 10);
+    voegStartProcesToe("C", 2, 8);
+    voegStartProcesToe("D", 3, 6);
+    voegStartProcesToe("E", 4, 4);
+    voegStartProcesToe("F", 5, 2);
+}
 
-    addProcess(key);
+function laadStartSituatieKleinNaarGroot() {
+    voegStartProcesToe("A", 0, 2);
+    voegStartProcesToe("B", 1, 4);
+    voegStartProcesToe("C", 2, 6);
+    voegStartProcesToe("D", 3, 8);
+    voegStartProcesToe("E", 4, 10);
+    voegStartProcesToe("F", 5, 12);
+}
+
+function laadStartSituatieKorteProcessen() {
+    voegStartProcesToe("A", 0, 5);
+    voegStartProcesToe("B", 0, 4);
+    voegStartProcesToe("C", 0, 8);
+    voegStartProcesToe("D", 2, 6);
+    voegStartProcesToe("E", 3, 3);
+    voegStartProcesToe("F", 5, 5);
+    voegStartProcesToe("G", 6, 2);
+    voegStartProcesToe("H", 6, 5);
+}
+
+function laadStartSituatieLangeProcessen() {
+    voegStartProcesToe("A", 0, 7);
+    voegStartProcesToe("B", 0, 9);
+    voegStartProcesToe("C", 0, 8);
+    voegStartProcesToe("D", 2, 5);
+    voegStartProcesToe("E", 3, 10);
+    voegStartProcesToe("F", 4, 4);
+    voegStartProcesToe("G", 4, 2);
+    voegStartProcesToe("H", 6, 5);
+}
+
+function laadStartSituatieAlleKleuren() {
+    voegStartProcesToe("A", 0, 7);
+    voegStartProcesToe("B", 1, 4);
+    voegStartProcesToe("C", 2, 2);
+    voegStartProcesToe("D", 3, 10);
+    voegStartProcesToe("E", 4, 1);
+    voegStartProcesToe("F", 5, 6);
+    voegStartProcesToe("G", 7, 3);
+    voegStartProcesToe("H", 20, 2);
+    voegStartProcesToe("I", 1, 4);
+    voegStartProcesToe("J", 2, 2);
+    voegStartProcesToe("K", 3, 10);
+    voegStartProcesToe("L", 4, 1);
+    voegStartProcesToe("M", 5, 6);
+    voegStartProcesToe("N", 7, 3);
+    voegStartProcesToe("O", 20, 2);
+    voegStartProcesToe("P", 1, 4);
+    voegStartProcesToe("Q", 2, 2);
+    voegStartProcesToe("R", 3, 10);
+    voegStartProcesToe("S", 4, 1);
+    voegStartProcesToe("T", 5, 6);
+    voegStartProcesToe("U", 7, 3);
+    voegStartProcesToe("V", 20, 2);
+    voegStartProcesToe("W", 3, 10);
+    voegStartProcesToe("X", 4, 1);
+    voegStartProcesToe("Y", 5, 6);
+    voegStartProcesToe("Z", 7, 3);
+}
+
+function voegStartProcesToe(process_id, start, length){
+    document.getElementById("dd_start_" + process_id).value = start;
+    document.getElementById("dd_length_" + process_id).value = length;
+
+    addProcess(process_id);
 }
 
 function runSchedulers () {
